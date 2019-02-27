9ab06b5629f6f56fa523f21cde8921e16ff877ac
nikita <nikitaostapenko@ukr.net>
2019-02-27 13:25:03 +0200

    Add brain-even game for the step4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import readlineSync from 'readline-sync';
 
 const startGame = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-}
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
 
-export default startGame;
+const checkOnEven = () => {
+  console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+
+  const numbersToCheck = [15, 6, 7];
+  const isEven = number => (number % 2 === 0 ? 'yes' : 'no');
+
+  for (let i = 0; i < numbersToCheck.length; i += 1) {
+    const answer = readlineSync.question(`Question: ${numbersToCheck[i]}\nYour answer: `);
+
+    if (isEven(numbersToCheck[i]) !== answer) {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${isEven(answer)}".\nLet's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export { startGame, checkOnEven };
