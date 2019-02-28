1672ada781ddc46b9987de03c7fc2439667849c7
nikita <nikitaostapenko@ukr.net>
2019-02-28 19:00:02 +0200

    Add brain-progression for the step7

--- src/games/even.js
+++ src/games/even.js
@@ -3,11 +3,10 @@ import getRandomInt from '../utils';
 
 const isEven = number => (number % 2 === 0);
 const getRightAnswer = number => (isEven(number) ? 'yes' : 'no');
-const roundQuestion = () => getRandomInt(0, 100);
 const gameMessage = 'Answer "yes" if number even otherwise answer "no".';
 
 const questionAnswerGenerator = () => {
-  const question = roundQuestion();
+  const question = getRandomInt(0, 100);
   const result = getRightAnswer(question);
   const questionAnswer = [question, result];
 
---
+++ src/games/progression.js
@@ -0,0 +1,35 @@
+import gameEngine from '../index';
+import getRandomInt from '../utils';
+
+const gameMessage = 'What number is missing in the progression?';
+const progressionLimit = 10;
+const getMissedNumber = (firstNumber, progressionStep, progressionHole) => {
+  let question = '';
+  let counter = firstNumber;
+  let result = 0;
+
+  for (let i = 0; i < progressionLimit; i += 1) {
+    if (i === progressionHole) {
+      question += (i === 0) ? '..' : ' ..';
+      result = counter;
+    } else {
+      question += (i === 0) ? `${String(counter)}` : ` ${String(counter)}`;
+    }
+    counter += progressionStep;
+  }
+
+  result = String(result);
+  return [question, result];
+};
+
+const questionAnswerGenerator = () => {
+  const firstNumber = getRandomInt(1, 50);
+  const progressionStep = getRandomInt(1, 50);
+  const progressionHole = getRandomInt(0, 9);
+  const [question, result] = getMissedNumber(firstNumber, progressionStep, progressionHole);
+  const questionAnswer = [question, result];
+
+  return questionAnswer;
+};
+
+export default () => gameEngine(gameMessage, questionAnswerGenerator);
