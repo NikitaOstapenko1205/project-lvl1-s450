e058a10e255501c67fa4fbd602c76a3881857564
nikita <nikitaostapenko@ukr.net>
2019-03-01 21:17:32 +0200

    fix brain-progression logic

--- src/games/progression.js
+++ src/games/progression.js
@@ -5,28 +5,25 @@ const gameMessage = 'What number is missing in the progression?';
 const progressionLimit = 10;
 const getQuestionAndAnswer = (firstNumber, progressionStep, progressionHole) => {
   let question = '';
-  let answer = 0;
 
   for (let i = 0; i < progressionLimit; i += 1) {
     const currentNumber = firstNumber + progressionStep * i;
     if (i === progressionHole) {
       question += ' ..';
-      answer = currentNumber;
     } else {
       question += ` ${String(currentNumber)}`;
     }
   }
 
-  question = question.trim();
-  answer = String(answer);
-  return [question, answer];
+  return question.trim();
 };
 
 const questionAndAnswerGenerator = () => {
   const firstNumber = getRandomInt(1, 50);
   const progressionStep = getRandomInt(1, 50);
   const progressionHole = getRandomInt(0, progressionLimit - 1);
-  const [question, answer] = getQuestionAndAnswer(firstNumber, progressionStep, progressionHole);
+  const question = getQuestionAndAnswer(firstNumber, progressionStep, progressionHole);
+  const answer = String(firstNumber + progressionStep * progressionHole);
   const questionAndAnswer = [question, answer];
 
   return questionAndAnswer;
